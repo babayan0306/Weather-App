30ca7c537309209c911ad586d6ced03599af776a
babayan0306 <125582118+babayan0306@users.noreply.github.com>
2023-12-15 19:39:30 +0400

    firt add

---
+++ Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Yurka Babayan on 15.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Yurka Babayan on 15.12.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
